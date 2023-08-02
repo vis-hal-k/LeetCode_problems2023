391485c2b47952e0b655513e6c10541b54fe8b80
Vishal Kumar <109166596+vis-hal-k@users.noreply.github.com>
2023-08-02 15:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (19.94%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin() , nums.end()) ;
+    void nextPermutation(vector<int>& arr) {
+        
+        int dip_index = -1 ;
+        int n = arr.size() ;
+        // First we find the dip index ;
+        for( int  i  = n-2 ; i >= 0 ; i-- ) {
+            if( arr[i+1] > arr[i]) {
+                dip_index = i ;
+                break ;
+            }
+        }
+        if(dip_index == -1 ) {
+            reverse(arr.begin() , arr.end()) ;
+            return  ; 
+        } 
+
+        for(int i = n-1  ; i > dip_index ; i--){
+            if(arr[i] > arr[dip_index]) {
+                swap(arr[i] , arr[dip_index]) ;
+                break ;
+            }
+        }
+        reverse(arr.begin()+dip_index+1, arr.end()) ;
         return ;
     }
 }; 
