ba161e9ddd143fcb87003dd292cac25cff808720
Vishal Kumar <109166596+vis-hal-k@users.noreply.github.com>
2023-08-06 09:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (12.26%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,17 +4,11 @@ public:
         int n = matrix.size() ;
         vector<vector<int>> arr(n , vector<int> (n , 1) ) ; 
           for(int j = 0; j < n  ; j++) {
-              for(int i = n-1, ii = 0  ; i >=0 || ii < n ; i-- , ii++){
+              for(int i = n-1, ii = 0  ; i >=0 ; i-- , ii++){
                   arr[j][ii] = matrix[i][j] ;   
               }
           }
           matrix = arr ;
-
-        // for(int i = 0; i < n ; i++) {
-        //       for(int j = 0  ; j < n ; j++  ){
-        //            matrix[i][j] =  arr[i][j];
-        //       }
-        //   }
          return ;
     }
 };
